acaf9c7fdd11be38ebcd517b3c5671cefffb0dfa
druiz18 <d.ruiz2257@gmail.com>
2021-06-15 12:35:13 -0500

    y

--- screenshotSelenium.py
+++ screenshotSelenium.py
@@ -11,29 +11,29 @@ class using_unittest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path=r"D:\chromeDriver\chromedriver_win32\chromedriver.exe")
 
-    def using_opencv(self):
+    def test_using_opencv(self):
         driver = self.driver
         driver.get("http://www.google.com")
         driver.save_screenshot('img2.png')
         time.sleep(3)
 
-    def compare (self):
+    def test_compare (self):
         img1 = cv2.imread('img1.png')
         img2 = cv2.imread('img2.png')
 
-        difference = cv2.absdiff(img1, img2)
-        image_gray = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
-        contours,_ = cv2.findContours(image_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        diferencia = cv2.absdiff(img1, img2)
+        imagen_gris = cv2.cvtColor(diferencia,cv2.COLOR_BGR2GRAY)
+        contours,_ = cv2.findContours(imagen_gris,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
         for c in contours:
             if cv2.contourArea(c) >= 20:
-                posicion_x, posicion_y, ancho, alto = cv2.boundingRect(c)
-                cv2.rectangle(img1(posicion_x, posicion_y), (posicion_x+ancho, posicion_y+alto), (0,0,255), 2)
+                posicion_x,posicion_y,ancho,alto = cv2.boundingRect(c)
+                cv2.rectangle(img1(posicion_x,posicion_y),(posicion_x+ancho,posicion_y+alto),(0,0,255),2)
 
         while(1):
-            cv2.imshow('image1', img1)
-            cv2.imshow('image2', img2)
-            cv2.imshow('difference detected', difference)
+            cv2.imshow('image1',img1)
+            cv2.imshow('image2',img2)
+            cv2.imshow('difference detected',diferencia)
             teclado = cv2.waitKey(5) & 0xFF
             if teclado == 27:
                 break
