23bb0ff385ac7077209bdff8a735bb8d1a2865ea
druiz18 <d.ruiz2257@gmail.com>
2021-06-16 13:08:36 -0500

    y

--- imagen.py
+++ imagen.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 #import os
 import time
-import matplotlib 
+
 #from PIL import Image, ImageChops
 #os.chdir(r"D:\Imagenes")
 img1 = cv2.imread('imge2.png', 0)
 img2 = cv2.imread('img2.png', 0)
 
-imas = np.hstack((img1, img2))
-cv2.imshow('imagenes', imas)
+cv2.imshow('Foto', img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-time.sleep(30)
--- screenshotSelenium.py
+++ screenshotSelenium.py
@@ -14,18 +14,44 @@ class using_unittest(unittest.TestCase):
 
     def test_using_opencv(self):
         driver = self.driver
-        driver.get("http://www.google.com")
+        driver.get("http://www.youtube.com")
         driver.save_screenshot('img2.png')
         time.sleep(3)
         assert "no se encontró el elemento: " not in driver.page_source
 
     def test_compare(self):
-        img1 = Image.open('img1.png')
-        img2 = Image.open('img2.png')
+        imagen1 = cv2.imread("img1.png", 1)
+        imagen2 = cv2.imread("img2.png", 1)
 
-        diferencia = ImageChops.difference(img1, img2)
-        if diferencia.getbbox():
-            diferencia.show()
+        newimg1 = cv2.resize(imagen1, (500, 300))
+        newimg2 = cv2.resize(imagen2, (500, 300))
+
+        diferencia = cv2.subtract(newimg1, newimg2)
+
+        #definir funcion de comparacion
+        def compara (im1, im2):
+	        diferencia = cv2.subtract(im1, im2)
+	        if not np.any(diferencia):
+		        print("Las imagenes son iguales")
+	        else:
+		        print("Las imagenes son diferentes")
+        #mostrar las dos imagenes en pantalla
+        imas = np.hstack((newimg1, newimg2))
+        cv2.imshow("fotos", imas)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        #crear imagen de diferencia
+        cv2.imwrite("imDiferencia.png", diferencia) 
+        cv2.imshow("diferencia", diferencia)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        # img1 = Image.open('img1.png', 0)
+        # img2 = Image.open('img2.png', 0)
+
+        # diferencia = ImageChops.difference(img1, img2)
+        # if diferencia.getbbox():
+        #     diferencia.show()
 
         # imagen_gris = cv2.cvtColor(diferencia,cv2.COLOR_BGR2GRAY)
         # contours,_ = cv2.findContours(imagen_gris,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
