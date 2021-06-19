7764abbc26c523b36d25c2857e0bab5a08f3933a
druiz18 <d.ruiz2257@gmail.com>
2021-06-18 20:23:43 -0500

    y

--- screenshotSelenium.py
+++ screenshotSelenium.py
@@ -16,8 +16,9 @@ class using_unittest(unittest.TestCase):
         driver = self.driver
         driver.get("http://www.youtube.com")
         driver.save_screenshot('img2.png')
-        time.sleep(3)
-        assert "no se encontró el elemento: " not in driver.page_source
+        driver.implicitly_wait(3)
+        #time.sleep(3)
+        #assert "no se encontró el elemento: " not in driver.page_source
 
     def test_compare(self):
         imagen1 = cv2.imread("img1.png", 1)
@@ -35,6 +36,7 @@ class using_unittest(unittest.TestCase):
 		        print("Las imagenes son iguales")
 	        else:
 		        print("Las imagenes son diferentes")
+                
         #mostrar las dos imagenes en pantalla
         imas = np.hstack((newimg1, newimg2))
         cv2.imshow("fotos", imas)
@@ -46,26 +48,7 @@ class using_unittest(unittest.TestCase):
         cv2.imshow("diferencia", diferencia)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-        # img1 = Image.open('img1.png', 0)
-        # img2 = Image.open('img2.png', 0)
-
-        # diferencia = ImageChops.difference(img1, img2)
-        # if diferencia.getbbox():
-        #     diferencia.show()
-
-        # imagen_gris = cv2.cvtColor(diferencia,cv2.COLOR_BGR2GRAY)
-        # contours,_ = cv2.findContours(imagen_gris,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        # for c in contours:
-        #     if cv2.contourArea(c) >= 20:
-        #         posicion_x,posicion_y,ancho,alto = cv2.boundingRect(c)
-        #         cv2.rectangle(img1(posicion_x,posicion_y),(posicion_x+ancho,posicion_y+alto),(0,0,255),2)
-
-        # while(1):
-        #     cv2.imshow('imagen1', img1)
-        #     cv2.imshow('imagen2', img2)
-        #     cv2.imshow('diferencia detectada', diferencia)
-        #     cv2.waitKey(0)
-        #     #cv2.destroyAllWindows()
+        
         
 if __name__=='__main__':
     unittest.main()
